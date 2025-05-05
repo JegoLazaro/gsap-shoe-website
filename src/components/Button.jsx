f5216ab92d59671c12903ab80ef8f472bbaf7a65
jego <josegabriel@boom.ai>
2025-05-06 01:39:59 +0800

    add and implement gsap animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Button = ({title}) => {
+const Button = ({title, id, rightIcon, leftIcon, containerClass}) => {
   return (
-    <div>{title}</div>
+    <button className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 ${containerClass}`} id={id}>
+        {leftIcon} 
+        <span className='relative incline-flex overflow-hidden font-circular-web text-xs uppercase'>
+            <div>
+                {title}
+            </div>
+        </span>
+        
+    </button>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import Button from "./Button";
-import { TiLocationArrow } from "react-icons/ti";
+import { FaArrowRight } from "react-icons/fa";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -16,6 +18,33 @@ const Hero = () => {
     setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
   };
 
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
   const getVideoSrc = (index) => `videos/jordan${index}.mp4`;
 
   const handleVideoLoad = () => {
@@ -28,10 +57,10 @@ const Hero = () => {
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg"
       >
-        <div className=" absolute-center absolute z-50 w-[900px] h-[900px] cursor-pointer overflow-hidden rounded-lg">
+        <div className="absolute-center absolute z-50 w-[900px] h-[900px] cursor-pointer overflow-hidden rounded-lg">
           <div
             onClick={handleMiniVideoClick}
-            className="origin-center flex w-full h-full cursor-pointer items-center justify-center rounded-lg bg-transparent scale-25 opacity-0 transition-all duration-500 ease-in hover:scale-50 hover:opacity-100"
+            className="origin-center flex w-full h-full cursor-pointer items-center justify-center rounded-lg scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
           >
             <video
               ref={nextVideoRef}
@@ -39,7 +68,7 @@ const Hero = () => {
               loop
               muted
               id="current-video"
-              className="clipped-image w-full h-full object-cover object-center transition-transform duration-500 ease-in-out"
+              className="clipped-image w-full h-full origin-center object-cover object-center transition-transform duration-500 ease-in-out"
               onLoadedData={handleVideoLoad}
             />
           </div>
@@ -50,17 +79,17 @@ const Hero = () => {
           src={getVideoSrc(currentIndex)}
           loop
           muted
-          // autoPlay
+          autoPlay
           id="next-video"
-          className="abosulute-center invisible absolute z-20 size-64 object-cover object-center"
+          className="absolute-center clipped-image invisible origin-center absolute z-20 size-64 object-cover object-center"
           onLoadedData={handleVideoLoad}
         />
         <video
           src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
           loop
           muted
-          // autoPlay
-          className="abosulute left-0 top-0 size-full object-cover object-center"
+          autoPlay
+          className="absolute left-0 top-0 size-full object-cover object-center"
         />
       </div>
       {/* <h1 className="special-font hero-heading absolute top-5 right-5 z-40 text-blue-75">
@@ -81,8 +110,8 @@ const Hero = () => {
 
       <div className="absolute bottom-5 left-5 z-40">
         <div className="m5-24 px-5 sm:px-10">
-          <div className="flex items-center gap-x-2">
-            <p className="mb-2 font-semibold text-lg text-blue-75 font-robert-regular">
+          <div className="flex items-center justify-between gap-x-2">
+            <p className="mb-2 font-semibold text-lg text-blue-75 pl-5 font-robert-regular">
               <span className="text-red-500 ">Defy</span> Gravity.
               <br /> <span className="text-red-500 ">Define</span> Greatness.
               <br /> <span className="text-red-500 ">Defend</span> the Legacy.
@@ -90,8 +119,8 @@ const Hero = () => {
             <Button
               id="watch-trailer"
               title="Watch Video"
-              leftIcon={<TiLocationArrow />}
-              containerClass="bg-blue-100 flex-center gap-1"
+              leftIcon={<FaArrowRight />}
+              containerClass="bg-blue-100 flex-center gap-2 w-1/2 font-robert-regular"
             />
           </div>
 
